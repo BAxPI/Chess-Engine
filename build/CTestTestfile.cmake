907e55b15efc84dc66da7d7c8e933ff1e4bfece8
BAxPI <baramber33@gmail.com>
2023-04-22 21:40:42 +0300

    Removed unnecessary folders

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/baramber/Desktop/Portfolio/CPP/chess/Chess-Engine
-# Build directory: /home/baramber/Desktop/Portfolio/CPP/chess/Chess-Engine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
